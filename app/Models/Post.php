1faa83783e44d4dd77aee4e9f4d5347a856c034d
Celine <chepceline25@gmail.com>
2023-09-04 23:11:13 +0300

    add belongs to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,8 +25,8 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function comment(){
-        return $this->belongsTo(Comment::class);
+    public function comments(){
+        return $this->hasMany(Comment::class);
     }
 
 }
