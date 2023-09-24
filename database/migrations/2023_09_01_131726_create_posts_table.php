f238987088575e335878ce3bc5e672e3b2e7a328
Celine <chepceline25@gmail.com>
2023-09-24 17:21:12 +0300

    add blog category migrations

--- database/migrations/2023_09_01_131726_create_posts_table.php
+++ database/migrations/2023_09_01_131726_create_posts_table.php
@@ -22,8 +22,8 @@ return new class extends Migration
             $table->timestamps();
 
             //Foreign Keys
-            $table->foreign('user_id')->on('users')->references('id')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreign('category_id')->on('categories')->references('id')->cascadeOnDelete()->cascadeOnUpdate();
+           // $table->foreign('user_id')->on('users')->references('id')->cascadeOnDelete()->cascadeOnUpdate();
+            //$table->foreign('category_id')->on('categories')->references('id')->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
 
